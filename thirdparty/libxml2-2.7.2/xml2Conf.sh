87097a1dde493805f26b3fcc607cdf3c735ac490
dengzijian <dengzijian@casachina.com.cn>
2018-03-13 10:53:59 +0800

    modify install file name

--- thirdparty/libxml2-2.7.2/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/dengzijian/hms_simulator/thirdparty/libxml2-2.7.2/../../libs/bin/libxml2-2.7.2/generic/i686-linux/lib"
-XML2_LIBS="-lxml2    -lm "
-XML2_INCLUDEDIR="-I/home/dengzijian/hms_simulator/thirdparty/libxml2-2.7.2/../../libs/bin/libxml2-2.7.2/generic/i686-linux/include/libxml2"
-MODULE_VERSION="xml2-2.7.2"
-
